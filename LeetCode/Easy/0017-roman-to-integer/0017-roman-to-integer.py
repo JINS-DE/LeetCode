d1f03db039f77ae92220b30bf597adb84267634a
Deon <115610267+JINS-DE@users.noreply.github.com>
2025-09-09 20:32:08 +0900

    Time: 0 ms (100%), Space: 17.8 MB (64.36%) - LeetHub

--- LeetCode/Easy/0017-roman-to-integer/0017-roman-to-integer.py
+++ LeetCode/Easy/0017-roman-to-integer/0017-roman-to-integer.py
@@ -1,15 +1,23 @@
+# 1. 일단 li =[M,CM,XC]로 저장하고
+# 2. 특징대로 빼기 더하기 해서 총합을 구하면됨
+
+value={'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000}
+
 class Solution:
-    def romanToInt(self, s: str) -> int:
-        hash = {'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000}
-        n=len(s)
-        prev=0
-        answer=0
-        for i in range(n-1,-1,-1):
-            now = hash[s[i]]
-            if now < prev:
-                answer-=now
+    def romanToInt(self, s: str) -> int:        
+        # s[::-1] test해보자
+        answer = 0
+        before = 0
+        for ch in s[::-1]:
+            cur=value[ch]
+            if cur < before:
+                answer-=cur
             else:
-                answer+=now
-            prev = now
+                answer+=cur
+            before = cur
         return answer
+
+
+
+
         
