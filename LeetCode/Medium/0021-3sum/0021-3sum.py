bf4e23b2de9cee9b650c60fd4970b2d139ea036a
Deon <115610267+JINS-DE@users.noreply.github.com>
2025-09-16 20:33:33 +0900

    Time: 612 ms (34.79%), Space: 19.8 MB (99.91%) - LeetHub

--- LeetCode/Medium/0021-3sum/0021-3sum.py
+++ LeetCode/Medium/0021-3sum/0021-3sum.py
@@ -1,21 +1,32 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        nums.sort()
-        answer=set()
-        for i in range(len(nums)-2):
-            j=i+1
-            k=len(nums)-1
-            while j<k:
-                three_sums = nums[i] + nums[j] + nums[k]
-                if three_sums == 0:
-                    answer.add((nums[i],nums[j],nums[k]))
-                    j+=1
-                elif three_sums < 0:
-                    j+=1
+        nums.sort()  # 배열을 오름차순으로 정렬
+        result = []
+
+        for i in range(len(nums) - 2):
+            # 중복되는 값을 건너뛰기
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+
+            left, right = i+1, len(nums)-1
+            
+            while left < right:
+                total = nums[i] + nums[left] + nums[right]
+                if total == 0 :
+                    result.append([nums[i],nums[left],nums[right]])
+
+                    while left<right and nums[left]==nums[left+1]:
+                        left += 1
+                    while left<right and nums[right]==nums[right-1]:
+                        right-=1
+
+                    left+=1
+                    right-=1
+                
+                elif total < 0 :
+                    left+=1
                 else:
-                    k-=1
-
-        return list(map(list, answer)) 
-
-
+                    right-=1
         
+        return result
+
