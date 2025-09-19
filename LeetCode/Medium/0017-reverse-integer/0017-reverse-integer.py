1a6e6f82de0153c0a086b423bae7152180e2c051
Deon <115610267+JINS-DE@users.noreply.github.com>
2025-09-19 21:28:09 +0900

    Time: 43 ms (31.86%), Space: 17.7 MB (83.31%) - LeetHub

--- LeetCode/Medium/0017-reverse-integer/0017-reverse-integer.py
+++ LeetCode/Medium/0017-reverse-integer/0017-reverse-integer.py
@@ -1,14 +1,10 @@
 class Solution:
-
     def reverse(self, x: int) -> int:
-        if x==0:
-            return 0 
-        
-        if x>0:
-            answer = int(str(x)[::-1])
-        else:
-            answer = -int(str(x)[1:][::-1])
-
-        if -2**31 > answer or 2**31-1 < answer :
+        # 음수인지 판별
+        sign = -1 if x < 0 else 1
+        # 절댓값 뒤집기
+        rev = int(str(abs(x))[::-1]) * sign
+        # 32-bit 범위 체크
+        if rev < -2**31 or rev > 2**31 - 1:
             return 0
-        return answer
+        return rev
