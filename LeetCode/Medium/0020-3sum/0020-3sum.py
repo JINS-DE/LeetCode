3a0bdf8e54fa43fee5aa2d90b5ca94fe9e63c5a0
Deon <115610267+JINS-DE@users.noreply.github.com>
2025-10-04 18:42:27 +0900

    Time: 580 ms (55.99%), Space: 20.6 MB (87.17%) - LeetHub

--- LeetCode/Medium/0020-3sum/0020-3sum.py
+++ LeetCode/Medium/0020-3sum/0020-3sum.py
@@ -1,41 +1,29 @@
-"""
-정렬 후 고정된 nums[i] 기준으로 투 포인터 방식으로 3sum 구하기
-3sum = nums[i] + nums[j] + nums[k]
-- 3sum == 0 인 경우 j++ , k--
-- 3sum > 0 인 경우 k--
-- 3sum < 0 인 경우 j++
-
-중복된 값 건너뛰기
-
-"""
-
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
+        answer=[]
+        n=len(nums)
         nums.sort()
-        n = len(nums)
-        answer = []
-        for i in range(n-2):
-            if i>0 and nums[i-1]==nums[i]:
+        for pivot in range(n-2):
+            if pivot>0 and nums[pivot]==nums[pivot-1]:
                 continue
-            j = i+1
-            k= n-1
 
-            while j<k:
-                total = nums[i] + nums[j] + nums[k]
-                if total==0:
-                    answer.append([nums[i],nums[j],nums[k]])
-                    j+=1
-                    k-=1
-                    while j<k and nums[j]==nums[j-1]:
-                        j+=1
-                    while k>i and nums[k]==nums[k+1]:
-                        k-=1
-                elif total < 0:
-                    j+=1
+            left=pivot+1
+            right=n-1
+            while left<right:
+                three_sum=nums[pivot]+nums[left]+nums[right]
+                if three_sum<0:
+                    left+=1
+                elif three_sum>0:
+                    right-=1
                 else:
-                    k-=1
+                    answer.append([nums[pivot],nums[left],nums[right]])
+                    while left<right and nums[left]==nums[left+1]:
+                        left+=1
+                    while left<right and nums[right]==nums[right-1]:
+                        right-=1
+                    left+=1
+                    right-=1
+    
         return answer
 
-                
-
-        
+                  
