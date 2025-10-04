b349725a43e05984fb79e67c6efac04894c50895
Deon <115610267+JINS-DE@users.noreply.github.com>
2025-10-04 18:55:46 +0900

    Time: 45 ms (18.41%), Space: 17.8 MB (36.81%) - LeetHub

--- LeetCode/Medium/0017-reverse-integer/0017-reverse-integer.py
+++ LeetCode/Medium/0017-reverse-integer/0017-reverse-integer.py
@@ -1,19 +1,7 @@
 class Solution:
     def reverse(self, x: int) -> int:
-        result = 0
-        sign = -1 if x < 0 else 1
-        x = abs(x)
-
-        while x != 0:
-            digit = x % 10        # 마지막 자리 추출
-            x //= 10              # 마지막 자리 제거
-
-            # 오버플로우 체크 (32비트 범위 벗어나면 0)
-            if result > (2**31 - 1) // 10 or (
-                result == (2**31 - 1) // 10 and digit > 7
-            ):
-                return 0
-
-            result = result * 10 + digit
-
-        return result * sign
+        x = str(x)[::-1]
+        if x[-1]=='-':
+            return 0 if -int(x[:-1]) < -(2**31) else -int(x[:-1])
+        else:
+            return 0 if int(x)>(2**31)-1 else int(x)
