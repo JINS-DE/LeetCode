fd4ee961645a8d9215a7067457adcd200471e6e0
Deon <115610267+JINS-DE@users.noreply.github.com>
2025-10-07 17:23:22 +0900

    Time: 247 ms (55.85%), Space: 20.1 MB (82.43%) - LeetHub

--- LeetCode/Medium/0020-number-of-islands/0020-number-of-islands.py
+++ LeetCode/Medium/0020-number-of-islands/0020-number-of-islands.py
@@ -1,21 +1,28 @@
+from collections import deque
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         dr = [-1,1,0,0]
         dc = [0,0,-1,1]
-        visited=[[0]*len(grid[0]) for _ in range(len(grid))]
-        def dfs(r,c):
-            for i in range(4):
-                nr = dr[i] + r
-                nc = dc[i] + c
-                if 0<=nr<len(grid) and 0<=nc<len(grid[0]) and grid[nr][nc]=='1' and visited[nr][nc]==0:
-                    visited[nr][nc]=1
-                    dfs(nr,nc)
-        answer=0
-        for i in range(len(grid)):
-            for j in range(len(grid[0])):
-                if grid[i][j]=='1' and visited[i][j]==0 :
-                    visited[i][j]=1
-                    dfs(i,j)
+        rows, cols = len(grid), len(grid[0])
+        answer= 0 
+        
+        for i in range(rows):
+            for j in range(cols):
+                if grid[i][j]=='1':
                     answer+=1
 
+                    # BFS
+                    q = deque([(i,j)])
+                    grid[i][j]='0'
+
+                    while q:
+                        r,c = q.popleft()
+
+                        for k in range(4):
+                            nr = r + dr[k]
+                            nc = c + dc[k]
+
+                            if 0 <= nr < rows and 0 <= nc < cols and grid[nr][nc] == '1':
+                                q.append((nr,nc))
+                                grid[nr][nc]='0'
         return answer
