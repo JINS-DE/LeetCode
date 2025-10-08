ba9559a2fc7e563602342b2b396bfbcdac8f2532
Deon <115610267+JINS-DE@users.noreply.github.com>
2025-10-08 23:40:42 +0900

    Time: 1890 ms (17.17%), Space: 17.9 MB (95.74%) - LeetHub

---
+++ LeetCode/Medium/0017-longest-increasing-subsequence/0017-longest-increasing-subsequence.py
@@ -0,0 +1,16 @@
+"""
+dp 배열의 의미
+dp[i] : i번째 반드시 포함해서 만들 수 있는 가장 긴 부분 수열의 길이
+
+"""
+
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        n=len(nums)
+        dp=[1]*n
+        for i in range(n):
+            for j in range(i+1):
+                if nums[j]<nums[i]:
+                    dp[i] = max(dp[i],dp[j]+1)
+        
+        return max(dp)
