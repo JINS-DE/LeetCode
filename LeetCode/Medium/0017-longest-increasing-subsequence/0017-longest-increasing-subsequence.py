ca11eb76b1e72a3196ccab5eb259691f74dfb142
Deon <115610267+JINS-DE@users.noreply.github.com>
2025-10-15 21:02:35 +0900

    Time: 1784 ms (34.37%), Space: 17.9 MB (80.64%) - LeetHub

--- LeetCode/Medium/0017-longest-increasing-subsequence/0017-longest-increasing-subsequence.py
+++ LeetCode/Medium/0017-longest-increasing-subsequence/0017-longest-increasing-subsequence.py
@@ -9,8 +9,7 @@ class Solution:
         n=len(nums)
         dp=[1]*n
         for i in range(n):
-            for j in range(i+1):
-                if nums[j]<nums[i]:
+            for j in range(i):
+                if nums[i] > nums[j]:
                     dp[i] = max(dp[i],dp[j]+1)
-        
         return max(dp)
